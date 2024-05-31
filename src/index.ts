5f555d0a12bf32865ee8f362a2db0bf7f9dbff3f
Kisan <kisankumavat85@gmail.com>
2024-05-31 19:06:57 +0530

    Initial AI setup

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,11 @@
  */
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		const response = await env.AI.run('@cf/meta/llama-2-7b-chat-int8', {
+			prompt: 'What is the origin of the phrase Hello, World',
+		});
+
+		return new Response(JSON.stringify(response));
 	},
 };
